274a5000ec598d6410f94401f0e22d938f6dfd79
vonpupp <albert@haevas.com>
2017-05-25 21:23:03 +0000

    [ADD] Module skeleton

---
+++ payment_contract/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 HAEVAS
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Payment Contract',
+    'summary': """
+        Mark as paid invoices that are paid using contract_auto""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'HAEVAS,Odoo Community Association (OCA)',
+    'website': 'http://haevas.com',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
